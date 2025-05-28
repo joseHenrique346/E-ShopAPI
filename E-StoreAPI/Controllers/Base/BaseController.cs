025cfc22a230f6827dd24ca6be098b5e6f1d9b9d
Carlos <.>
2025-05-27 22:05:31 -0300

    ADD -> Created Generic Controllers

---
+++ E-StoreAPI/Controllers/Base/BaseController.cs
@@ -0,0 +1,111 @@
+﻿using Arguments.Argument.Base.Crud;
+using Arguments.Conversor;
+using Domain.DTO.Base;
+using Domain.Interface.Service.Base;
+using Infrastructure.Persistence.EFCore.UnitOfWork.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ProjetoTesteWebApiNovo.Controllers.Base
+{
+    public class BaseController<TService, TDTO, TOutput, TInputIdentityView, TInputCreate, TInputIdentityUpdate, TInputIdentityDelete> : Controller
+        where TService : IBaseService<TDTO, TOutput, TInputIdentityView, TInputCreate, TInputIdentityUpdate, TInputIdentityDelete>
+        where TDTO : BaseDTO
+        where TInputIdentityView : BaseInputIdentityView<TInputIdentityView>
+        where TInputCreate : BaseInputCreate<TInputCreate>
+        where TInputIdentityUpdate : BaseInputIdentityUpdate<TInputIdentityUpdate>
+        where TInputIdentityDelete : BaseInputIdentityDelete<TInputIdentityDelete>
+        where TOutput : BaseOutput<TOutput>, new()
+    {
+        #region Dependency Injection
+
+        private readonly TService _service;
+        private readonly IUnitOfWork _unitOfWork;
+        public BaseController(TService service, IUnitOfWork unitOfWork)
+        {
+            _service = service;
+            _unitOfWork = unitOfWork;
+        }
+
+        #endregion
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _unitOfWork.BeginTransaction();
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            _unitOfWork.Commit();
+            base.OnActionExecuted(context);
+        }
+
+        #region Get
+
+        [HttpGet("Get")]
+        public async Task<ActionResult<TOutput>> GetAll()
+        {
+            var getAll = await _service.GetAll();
+            if (getAll != null)
+                return Ok(Conversor.GenericConvertList<TOutput, TDTO>(getAll));
+            return BadRequest();
+        }
+
+        [HttpPost("GetListByListId")]
+        public async Task<ActionResult<TOutput>> GetListByListId(List<TInputIdentityView> listInputIdentityView)
+        {
+            var getListByListId = await _service.GetListByListId(listInputIdentityView);
+            if (getListByListId != null)
+                return Ok(Conversor.GenericConvertList<TOutput, TDTO>(getListByListId));
+            return BadRequest();
+        }
+
+        #endregion
+
+        #region Create
+
+        [HttpPost("Create/Multiple")]
+        public async Task<ActionResult<TOutput>> CreateMultiple(List<TInputCreate> listInputCreate)
+        {
+            var result = await _service.CreateMultiple(listInputCreate);
+
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        #endregion
+
+        #region Update
+
+        [HttpPut("Update/Multiple")]
+        public async Task<ActionResult<TOutput>> UpdateMultiple(List<TInputIdentityUpdate> listInputIdentityUpdate)
+        {
+            var result = await _service.UpdateMultiple(listInputIdentityUpdate);
+
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpDelete("Delete/Multiple")]
+        public async Task<ActionResult<TOutput>> DeleteMultiple(List<TInputIdentityDelete> listInputIdentityDelete)
+        {
+            var result = await _service.DeleteMultiple(listInputIdentityDelete);
+
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        #endregion
+    }
+}
---
+++ E-StoreAPI/Controllers/Registration/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using Arguments.Argument.Registration.Brand;
+using Domain.DTO.Entity.Brand;
+using Domain.Interface.Service.Brand;
+using Infrastructure.Persistence.EFCore.UnitOfWork.Interface;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoTesteWebApiNovo.Controllers.Base;
+
+namespace ProjetoTesteWebApiNovo.Controllers.Registration
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BrandController : BaseController<IBrandService, BrandDTO, OutputBrand, InputIdentityViewBrand, InputCreateBrand, InputIdentityUpdateBrand, InputIdentityDeleteBrand>
+    {
+        public BrandController(IBrandService service, IUnitOfWork unitOfWork) : base(service, unitOfWork) { }
+    }
+}
